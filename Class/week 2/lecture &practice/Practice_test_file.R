712d63c072be1c794dcebf4372703352b4b315d8
AmrithaP <52408359+AmrithaP@users.noreply.github.com>
2024-01-19 18:47:17 -0500

    Stats

---
+++ Class/week 2/lecture &practice/Practice_test_file.R
@@ -0,0 +1,10 @@
+?msleep
+view(msleep)
+summary(msleep)
+c <- "amritha"
+class(c)
+
+age <- c(28, 48, 47, 71, 22, 80, 48, 30, 31)
+purchase <- c(20, 59, 2, 12, 22, 160, 34, 34, 29)
+bookstore <- data.frame(age, purchase)
+View(bookstore)
